bf70d8a2d4611f6358f38009f31c96466fa52f00
J Wayston <177466602+jwayston@users.noreply.github.com>
2026-02-26 19:38:56 +0200

    Added support for depth of extraction

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
-# Nested outgoing links extractor plugin for [The Archive](https://zettelkasten.de/the-archive/)
+# Nested outgoing links outline extractor plugin for [The Archive](https://zettelkasten.de/the-archive/)
 
-Extracts and nests outgoing links for the link detected within a selected text.
+Extracts and nests outgoing links for the link detected in a selection. Iterates until the given depth is reached.
 
-It detects indentation level of the selection and indents the results accordingly.
+Link descriptions are derived from the filenames. If the filename consists of just a UID, the first detected H1 header will be used.
+
+**Example usage**
+
+Selected text:
+```
+The motivator for making a change can be pain or creativity [[250215S8]]
+```
+
+Output:
+```
+The motivator for making a change can be pain or creativity [[250215S8]]
+  - Is the pursuit of order a sign of a need for control or a desire for aesthetic pleasure? [[250128I2]]
+    - Art is the transformation of entropy into order [[250128IL]]
+    - It would be more natural for humanity to accept disorder than to strive for artificial order [[25020600]]
+  - The therapeutic examination of suffering is determined by the type of suffering [[240224L5]]
+    - Associating evil with suffering can leave a person responsible for their own suffering [[250129NY]]
+
+```
+
+## Possible issues
+
+Since my Zettelkasten uses single custom UIDs as filenames, only basic testing was done for traditional "DATETIMEUID File description" style filenames. Feel free to open an issue in case of problems. 
 
--- com.idonthavedomain.nestedlinks.thearchiveplugin/main.js
+++ com.idonthavedomain.nestedlinks.thearchiveplugin/main.js
@@ -7,83 +7,136 @@ Author: JW
 
 "use strict";
 
-const regexLink = new RegExp(/\[\[(.*)\]\]/);
-const regexNoteHeader = new RegExp("(?<=^|\\n)# (.+)\\n");
-const regexForwardLinks = new RegExp(/(\[\[.*\]\])/g);
+const INDENT = 2;
+const regexWikilinks = new RegExp(/(\[\[.*?\]\])/g);
+const regexWikilinkTarget = new RegExp(/\[\[(.*?)\]\]/);
 const regexIndent = new RegExp(/^(\s+)/);
 
-
-function getIndent(text)
-{
-	const indentMatch = text.match(regexIndent);
-
-	if (indentMatch === null)
-		return "";
-
-	return indentMatch[1];
-}
+/* 
++------------------------------------------------------------------------------+
+|                             <header extraction>                              |
++------------------------------------------------------------------------------+
+*/
+const currentNote = input.notes.selected[0].filename;
+const regexIdDesc = new RegExp(/([0-9]{12,}|[0-9]{6,}.*?(?=\s|$)|.*?(?=\s|$))\s?(.*)/);
+const regexNoteHeader = new RegExp("(?<=^|\\n)# (.+)\\n");
 
 function extractHeader(noteContent)
 {
-	const headerMatch = noteContent.match(regexNoteHeader);
-	return headerMatch === null ? "<No H1 header detected>" : headerMatch[1];
+    const headerMatch = noteContent.match(regexNoteHeader);
+    return !headerMatch ? "<No H1 header detected>" : headerMatch[1];
 }
 
-function extractNoteHeader(noteId)
+function extractNoteHeaderLinkPair(linkText, excludeFilePattern)
 {
-	const note = app.search(noteId, false).bestMatch;
-	return note === null ? "<Note not found>" : extractHeader(note.content);
+    let matchIdDesc;
+    let noteFound = false;
+    const searchBestMatch = app.search(linkText).bestMatch;
+
+    if (searchBestMatch)
+        if(!searchBestMatch.filename.match(excludeFilePattern))
+            noteFound = true;
+
+    if (noteFound)
+        matchIdDesc = searchBestMatch.filename.match(regexIdDesc);
+    else
+        return null;
+
+    if (!matchIdDesc)
+        return `<Error: cannot extract any remotely ID-like from the link> ${selection}`;
+
+    const id = matchIdDesc[1];
+    const desc = matchIdDesc[2];
+
+    if (!desc && noteFound)
+        return `${extractHeader(searchBestMatch.content)} [[${id}]]`;
+
+    return `${desc} [[${id}]]`;
+}
+/* 
++------------------------------------------------------------------------------+
+|                             </header extraction>                             |
++------------------------------------------------------------------------------+
+*/
+
+function getIndent(text)
+{
+    const indentMatch = text.match(regexIndent);
+    return !indentMatch ? "" : indentMatch[1];
 }
 
 function extractLinkTarget(text)
 {
-	const linkMatch = text.match(regexLink);
-	return linkMatch === null ? null : linkMatch[1];
+    const linkMatch = text.match(regexWikilinkTarget);
+    return !linkMatch ? linkMatch : linkMatch[1];
 }
 
-function findForwardLinks(noteId)
+function findOutgoingLinks(noteId, excludeNoteId)
 {
-	const note = app.search(noteId, false).bestMatch;
+    const note = app.search(noteId, false).bestMatch;
+    if (!note)
+        return null;
 
-	if (note === null)
-		return null;
-
-	// Skip currently open note
-	if (note.filename === input.notes.selected[0].filename)
-		return "";
-
-	const forwardLinksMatch = note.content.match(regexForwardLinks);
-	if (forwardLinksMatch === null)
-		return null;
-
-	return forwardLinksMatch;
+    const matchOutgoingLinks = note.content.match(regexWikilinks);
+    return !matchOutgoingLinks ? null : matchOutgoingLinks;
 }
 
-function extractIndentedForwardLinks(textLine)
+function extractNestedOutgoingLinks(textLine, depth)
 {
-	const initialIndent = getIndent(textLine);
-	const targetNoteId = extractLinkTarget(textLine);
-	let outputText = "";
+    if (depth === 0)
+        return "";
 
-	if (targetNoteId === null)
-		return `${textLine} <Couldn't extract target note ID from the link>`;
+    let outputText = "";
+    const initialIndent = getIndent(textLine);
+    const linkTarget = extractLinkTarget(textLine);
 
-	const forwardLinks = findForwardLinks(targetNoteId);
+    if (!linkTarget)
+        return `${textLine} <Couldn't extract link target>`;
 
-	if (forwardLinks === null)
-		return textLine;
+    const outgoingLinks = findOutgoingLinks(linkTarget);
+    if (!outgoingLinks)
+        return outputText;
 
-	outputText += textLine;
-	outputText += textLine.slice(-1) !== "\n" ? "\n" : "";
+    uidSet.add(linkTarget);
 
-	for (let link of forwardLinks)
-	{
-		const linkId = extractLinkTarget(link);
-		const linkHeader = extractNoteHeader(linkId);
-		outputText += `${initialIndent}  - ${linkHeader} ${link}\n`;
-	}
+    // Go through detected outgoing links
+    for (let outLink of outgoingLinks)
+    {
+        const outLinkTarget = extractLinkTarget(outLink);
+        const linkExpanded = extractNoteHeaderLinkPair(outLinkTarget, linkTarget);
 
-	return outputText;
+        // Do not repeat outgoing link if it was already shown before
+        if (uidSet.has(outLinkTarget))
+            continue;
+
+        // Skip non-working links
+        if (!linkExpanded)
+            continue;
+
+        const outputLine = `${initialIndent}${" ".repeat(INDENT)}- ${linkExpanded}\n`;
+        outputText += outputLine;
+        outputText += extractNestedOutgoingLinks(outputLine, depth - 1);
+        uidSet.add(outLinkTarget);
+    }
+
+    return outputText;
 }
 
-output.insert.text = extractIndentedForwardLinks(input.text.selected);
+
+let uidSet = new Set();  // Keep track of visited notes
+let outputText = input.text.selected;
+
+const depth = parseInt(app.prompt({
+    title: "Depth",
+    description: "How many levels deep do you want to dive in?",
+    placeholder: "Depth",
+    defaultValue: 4
+}));
+
+if (!Number.isInteger(depth) || depth < 1)
+    cancel("Given depth was not a valid integer value");
+
+outputText += outputText.slice(-1) !== "\n" ? "\n" : "";
+outputText += extractNestedOutgoingLinks(input.text.selected, depth);
+output.insert.text = outputText;
+
